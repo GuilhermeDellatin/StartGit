be83a47d0c10b12564164e5f410b8bcdf2f819f3
GuilhermeDellatin <guidellatin@gmail.com>
2019-09-08 15:07:09 -0300

    Mensagem de boa noite

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,9 +3,8 @@ package application;
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Bom Dia!!!!");
-        System.out.println("Boa Tarde!!!!");
-
+        System.out.println("Bom Dia!!!!!");
+        System.out.println("Boa Tarde!!!");
+        System.out.println("Boa noite!!!");
     }
 }
